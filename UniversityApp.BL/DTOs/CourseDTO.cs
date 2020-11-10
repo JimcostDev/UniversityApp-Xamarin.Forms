81891c89cc619cf646a7095119491c3c6e856405
JimcostDev <rjak727@gmail.com>
2020-11-09 19:28:58 -0500

    Configurando estructura del proyecto

---
+++ UniversityApp.BL/DTOs/CourseDTO.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversityApp.BL.DTOs
+{
+    class CourseDTO
+    {
+        [JsonProperty("CourseID")]
+        public long CourseID { get; set; }
+
+        [JsonProperty("Title")]
+        public string Title { get; set; }
+
+        [JsonProperty("Credits")]
+        public long Credits { get; set; }
+    }
+}
