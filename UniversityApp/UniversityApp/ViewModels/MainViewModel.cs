622184c3bdc737f9b11dc3aef75f0191304da36c
JimcostDev <rjak727@gmail.com>
2020-11-12 17:53:05 -0500

    Update MainViewModel, add CourseInstructorsModel

--- UniversityApp/UniversityApp/ViewModels/MainViewModel.cs
+++ UniversityApp/UniversityApp/ViewModels/MainViewModel.cs
@@ -7,9 +7,12 @@ namespace UniversityApp.ViewModels
     public class MainViewModel
     {
         public CourseViewModel Courses { get; set; }
+        public CourseInstructorsViewModel CourseInstructors { get; set; }
+
         public MainViewModel()
         {
             Courses = new CourseViewModel();
+            CourseInstructors = new CourseInstructorsViewModel();
         }
     }
 }
